4f9593e79910b11c93b3d17fc0778d23e391b6ac
SimonAdrian <simon.adrian.schneider@gmail.com>
2023-09-01 16:33:55 +0200

    Initializing DFD class

---
+++ core/DFD.py
@@ -0,0 +1,21 @@
+
+
+
+class CDFD:
+    """Class CDFD as central collection of all extracted information.
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"DFD {name}"
+
+    def add_service(self, service):
+        pass
+
+    def add_external_entity(self, external_entity):
+        pass
+
+    def add_information_flow(self, information_flow):
+        pass
